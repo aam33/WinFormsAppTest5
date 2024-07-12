50b5b32a4f61bcf9147d4dc62362be5799dce820
aam33 <aam33@calvin.edu>
2024-07-12 18:38:23 -0400

    make the spotify player functional and add album player

--- AlbumBand.cs
+++ AlbumBand.cs
@@ -3,7 +3,7 @@
     // TEST
     internal class AlbumBand
     {
-        public int AlbumID { get; set; }
+        //public int AlbumID { get; set; }
         public String Artist { get; set; }
         public String AlbumTitle { get; set; }
         public int ReleaseYear { get; set; }
@@ -12,6 +12,7 @@
         //public String BandName { get; set; }
         //public int YearFounded { get; set; }
         //public int SingerID { get; set; }     // TEST STEP 2
+        public String AlbumURL { get; set; }
 
     }
     // END TEST - THIS ENTIRE FILE TO BE COMMENTED OUT/DELETED IF DOESN'T WORK
